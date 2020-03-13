4dccbb561cd57bd23427c5a9dd961036ef9d3e75
tigergao99 <tigergao1999@outlook.com>
2020-03-13 14:58:13 -0400

    update

--- testbuild.go
+++ testbuild.go
@@ -18,8 +18,49 @@ type Configuration struct {
 	Jailname  string
 }
 
-func connectSSH(username, hostname, port, giturl,
-	keypath, jailname string) {
+commands1 = []string{
+	fmt.Sprintf("yes | poudriere jail -d -j %s", jailname),
+	fmt.Sprintf("poudriere jail -c -j %s -m git -b -v master -U %s -K GENERIC", jailname, giturl),
+	fmt.Sprintf("poudriere bulk -j %s -p default -f pkglist", jailname),
+	fmt.Sprintf("poudriere image -j %s -f pkglist -t tar -p default", jailname),
+	"./script/check_charlie",
+	"cd /b/tftpboot/FreeBSD/install",
+	"chflags noschg lib/*",
+	"rm -rf lib",
+	"chflags noschg usr/lib32/*",
+	"rm -rf usr/lib32",
+	"chflags noschg libexec/*",
+	"rm -rf libexec",
+	"chflags noschg usr/bin/*",
+	"rm -rf bin",
+	"chflags noschg sbin/*",
+	"rm -rf sbin",
+	"chflags noschg var/*",
+	"rm -rf var",
+	"chflags noschg usr/lib/*",
+	"rm -rf usr/lib",
+	"rm -rf *",
+	"rm -rf .*",
+	"cp /data/images/poudriereimage.txz /b/tftpboot/FreeBSD/install",
+	"tar -xf /data/images/poudriereimage.txz -C /b/tftpboot/FreeBSD/install",
+	"mkdir -p /b/tftpboot/FreeBSD/install/conf/",
+	"{ mkdir -p conf/base; tar -c -v -f conf/base/etc.cpio.gz --format cpio --gzip etc; tar -c -v -f conf/base/var.cpio.gz --format cpio --gzip var; } | chroot .",
+	"cp -r ~/conf/default /b/tftpboot/FreeBSD/install/conf/",
+	"mkdir /b/tftpboot/FreeBSD/install/root/.ssh",
+	"cp ~/.ssh/id_rsa.pub /b/tftpboot/FreeBSD/install/root/.ssh/authorized_keys",
+	"{ echo 1; echo \"on 6\"; sleep 1; } | telnet 192.168.11.99",
+	"exit",
+}
+
+commands2 = []string{
+	"pwd_mkdb -p /etc/master.passwd",
+	"cd /usr/tests",
+	"kyua test",
+	"exit",
+}
+
+func connectSSH(username, hostname, giturl,
+	keypath, jailname string, commands []string) {
 	// SSH client config
 	key, err := ioutil.ReadFile(keypath)
 	signer, err := ssh.ParsePrivateKey(key)
@@ -37,7 +78,7 @@ func connectSSH(username, hostname, port, giturl,
 	
 
 	// Connect to host
-	client, err := ssh.Dial("tcp", hostname+":"+port, config)
+	client, err := ssh.Dial("tcp", hostname, config)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -67,50 +108,7 @@ func connectSSH(username, hostname, port, giturl,
 		log.Fatal(err)
 	}
 	
-	var commands []string
 	// send the commands
-	if hostname == "192.168.11.101" {
-		commands = []string{
-			fmt.Sprintf("yes | poudriere jail -d -j %s", jailname),
-			fmt.Sprintf("poudriere jail -c -j %s -m git -b -v master -U %s -K GENERIC", jailname, giturl),
-			fmt.Sprintf("poudriere bulk -j %s -p default -f pkglist", jailname),
-			fmt.Sprintf("poudriere image -j %s -f pkglist -t tar -p default", jailname),
-			"./script/check_charlie",
-			"cd /b/tftpboot/FreeBSD/install",
-			"chflags noschg lib/*",
-			"rm -rf lib",
-			"chflags noschg usr/lib32/*",
-			"rm -rf usr/lib32",
-			"chflags noschg libexec/*",
-			"rm -rf libexec",
-			"chflags noschg usr/bin/*",
-			"rm -rf bin",
-			"chflags noschg sbin/*",
-			"rm -rf sbin",
-			"chflags noschg var/*",
-			"rm -rf var",
-			"chflags noschg usr/lib/*",
-			"rm -rf usr/lib",
-			"rm -rf *",
-			"rm -rf .*",
-			"cp /data/images/poudriereimage.txz /b/tftpboot/FreeBSD/install",
-			"tar -xf /data/images/poudriereimage.txz -C /b/tftpboot/FreeBSD/install",
-			"mkdir -p /b/tftpboot/FreeBSD/install/conf/",
-			"{ mkdir -p conf/base; tar -c -v -f conf/base/etc.cpio.gz --format cpio --gzip etc; tar -c -v -f conf/base/var.cpio.gz --format cpio --gzip var; } | chroot .",
-			"cp -r ~/conf/default /b/tftpboot/FreeBSD/install/conf/",
-			"mkdir /b/tftpboot/FreeBSD/install/root/.ssh",
-			"cp ~/.ssh/id_rsa.pub /b/tftpboot/FreeBSD/install/root/.ssh/authorized_keys",
-			"{ echo 1; echo \"on 6\"; sleep 1; } | telnet 192.168.11.99",
-			"exit",
-		}
-	} else {
-		commands = []string{
-			"pwd_mkdb -p /etc/master.passwd",
-			"cd /usr/tests",
-			"kyua test",
-			"exit",
-		}
-	}
 	for _, cmd := range commands {
 		_, err = fmt.Fprintf(stdin, "%s\n", cmd)
 		if err != nil {
@@ -124,7 +122,7 @@ func connectSSH(username, hostname, port, giturl,
 
 func LoadConfiguration() Configuration {
 	var config Configuration
-    configFile, err := os.Open("gotest.json")
+    configFile, err := os.Open("test.json")
     defer configFile.Close()
     if err != nil {
         fmt.Println(err.Error())
@@ -136,9 +134,10 @@ func LoadConfiguration() Configuration {
  
 func main() {
 	configuration := LoadConfiguration()
-	connectSSH("root", "192.168.11.101", "22", 
-		configuration.Giturl, configuration.Privkey, configuration.Jailname)
+	print(configuration.Privkey)
+	connectSSH("root", "noblerock.zapto.org:10022", 
+		configuration.Giturl, configuration.Privkey, configuration.Jailname, commands1)
 	print("Waiting for charlie to boot up!\n")
 	time.Sleep(150 * time.Second)
-	connectSSH("root", "192.168.11.4", "22", "", configuration.Privkey, "")
+	connectSSH("root", "localhost:20022", "", configuration.Privkey, "", commands2)
 }
